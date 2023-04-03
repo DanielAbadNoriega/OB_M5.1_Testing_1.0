fa6926c310b3fea5293d1b44ee6dc5456909d0c3
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2023-04-03 19:38:53 +0200

    [ app.test.js ] fixed var r to screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,50 +1,56 @@
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 import App from "./App";
 import InputNuevaNota from "./components/InputNuevaNota";
 import ListadoNotas from './components/ListadoNotas'
 
 describe('REACT - Testeamos los componentes', () => {
     test('El listado se renderiza correctamente', () => {
-        const r = render(<ListadoNotas />);
-        expect(r).toBeDefined();
+        const view = render(<ListadoNotas />);
+        expect(view).toBeDefined();
     })
     test('El listado renderiza un listado correctamente', () => {
         const notas = ["bajar la basura", "Comprar huevos"];
-        const r = render(<ListadoNotas notas={notas} />);
-        expect(r).toBeDefined();
+        const view = render(<ListadoNotas notas={notas} />);
+        expect(view).toBeDefined();
     })
+    /* test('El listado renderiza sólo las notas que debe renderizar', () => {
+        const notas = ["bajar la basura", "Comprar huevos"];
+        const view = render(<ListadoNotas notas={notas} />);
+        const div = view.getByLabelText('listado-notas');
+        expect(div.childElementCount).toBe(2);
+    }) */
     test('El listado renderiza sólo las notas que debe renderizar', () => {
         const notas = ["bajar la basura", "Comprar huevos"];
-        const r = render(<ListadoNotas notas={notas} />);
-        const div = r.getByLabelText('listado-notas');
+        render(<ListadoNotas notas={notas} />);
+        const div = screen.getByLabelText('listado-notas');
         expect(div.childElementCount).toBe(2);
-    })
+      })
 })
 
 describe('REACT - Hacemos un test de integración', () => {
     test('Renderizamos la app', () => {
-        const r = render(<App />)
-        expect(r).toBeDefined();
+        const view = render(<App />)
+        expect(view).toBeDefined();
     })
     test('Se renderiza el input', () => {
         const placeholdertext = "Introduce una nueva nota";
-        const r = render(<App />)
-        const input = r.getByPlaceholderText(placeholdertext);
+        render(<App />)
+        const input = screen.getByPlaceholderText(placeholdertext);
         expect(input).toBeDefined();
     })
     test('Cuando hacemos clic en el botón Añadir, se lanza el evento', () => {
         const funcionMock = jest.fn();
-        const r = render(<InputNuevaNota addNuevaNota={funcionMock} />);
-        const button = r.getByText("Añadir");
+        render(<InputNuevaNota addNuevaNota={funcionMock} />);
+        const button = screen.getByText("Añadir");
         fireEvent.click(button);
         expect(funcionMock).toHaveBeenCalledTimes(1);
     })
     test('Añadimos una nueva nota', () => {
         const placeholdertext = "Introduce una nueva nota";
-        const r = render(<App />)
-        const input = r.getByPlaceholderText(placeholdertext);
-        const button = r.getByText("Añadir");
-        const div = r.getByLabelText('listado-notas');
+        render(<App />)
+        const input = screen.getByPlaceholderText(placeholdertext);
+        const button = screen.getByText("Añadir");
+        const div = screen.getByLabelText('listado-notas');
         const hijosInicial = div.childElementCount;
         fireEvent.change(input, { target: { value: 'Poner gasolina' } });
         fireEvent.click(button);
