67dbf197159b5d6b57af3baaf2d983d7200ae895
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2023-04-03 19:26:43 +0200

    Iteration: modified input

--- src/components/InputNuevaNota.jsx
+++ src/components/InputNuevaNota.jsx
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 const InputNuevaNota = ({ addNuevaNota }) => {
   const [nuevaNota, setNuevaNota] = useState("");
+
+  const setNewTask = () => {
+    addNuevaNota(nuevaNota);
+    setNuevaNota("");
+  };
+
   return (
     <div className="input-nueva-nota">
       <input
         type="text"
         placeholder="Introduce una nueva nota"
         value={nuevaNota}
-        onKeyDown={(e) => e.key === "Enter" && addNuevaNota(nuevaNota)}
+        onKeyDown={(e) => e.key === "Enter" && setNewTask()}
         onChange={(e) => setNuevaNota(e.target.value)}
       />
-      <button onClick={() => addNuevaNota(nuevaNota)}>Añadir</button>
+      <button onClick={() => addNuevaNota(nuevaNota) && setNewTask()}>
+        Añadir
+      </button>
     </div>
   );
 };
--- src/components/ListadoNotas.jsx
+++ src/components/ListadoNotas.jsx
@@ -4,7 +4,7 @@ const ListadoNotas = ({ notas = [] }) => {
   return (
     <div className="lista-notas" aria-label="listado-notas">
       {notas.map((nota, i) => (
-        <div key={i}>{nota}</div>
+        <div key={i} style={{borderRadius: "5px"}}>{nota}</div>
       ))}
     </div>
   );
